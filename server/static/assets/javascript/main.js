7bc81323257f789915a0991ece1ec77e6b2bd197
r00t1000 <jacob.jime@gmail.com>
2016-11-03 14:35:59 -0700

    Vue is getting annoying

--- server/static/assets/javascript/main.js
+++ server/static/assets/javascript/main.js
@@ -14,10 +14,16 @@ var config = {
    db.ref('thoughts/'+url[1]).once('value', function(snapshot) {
      if (snapshot.hasChild('title')) {
        notie.alert(1, 'Success!' , 2)
+       document.title = "YourThoughts 💡" + ' - ' + snapshot.key;
        document.getElementById('mainView').innerHTML = '';
-       document.getElementById('mainView').innerHTML += '<h1 style="text-align: center; font-family:"CircularStdBold", sans-serif;" class="u-full-width" id="postkey">'+snapshot.key+'</h1>';
-       document.getElementById('mainView').innerHTML += '<input class="u-full-width" type="text" value="'+snapshot.val().title+'" id="postTitle">'
+       document.getElementById('mainView').innerHTML += '<div class="three columns"> <p></p> </div>'
+       document.getElementById('mainView').innerHTML += '<div class="four columns"> <div style="margin: 0 auto; margin-top: 50%">'
+       document.getElementById('mainView').innerHTML += '<input class="u-full-width" v-model="thoughtId" disabled type="text" value="'+snapshot.key+'">'
+       document.getElementById('mainView').innerHTML += '<input class="u-full-width" v-model="thoughtTitle" type="text" value="'+snapshot.val().title+'" id="postTitle">'
        document.getElementById('mainView').innerHTML += '<textarea class="u-full-width" type="text" value="'+snapshot.val().post+'" id="post">'
+       document.getElementById('mainView').innerHTML += '<input id="submitNewInfo" style="background-color: #FF1744; border-color: #FF1744;" v-on:click="submitNewInfo" class="button-primary u-full-width" type="submit" value="Update">'
+       document.getElementById('mainView').innerHTML += '</div></div>'
+       document.getElementById('mainView').innerHTML += '<div class="three columns"> <p></p> </div>'
      }
    });
  }
@@ -31,8 +37,8 @@ var rootView = new Vue({
     thoughtTitle: '',
     thoughtAuthor: '',
     thoughtId: '',
-    uid: '',
-    htmlTitle: 'YourThoughts 💡'
+    thoughtPost: '',
+    uid: ''
   },
   methods: {
     createThought: function () {
@@ -52,7 +58,18 @@ var rootView = new Vue({
           });
           notie.alert(1, 'Success!', 2)
           document.getElementById('mainView').innerHTML = '';
-          document.getElementById('mainView').innerHTML = '<input class="u-full-width" type="text" value="'+newPostId+'" id="postTitle">'
+          db.ref('thoughts/'+newPostId).once('value', function(snapshot) {
+            document.title = "YourThoughts 💡" + ' - ' + snapshot.key;
+            document.getElementById('mainView').innerHTML = '';
+            document.getElementById('mainView').innerHTML += '<div class="three columns"> <p></p> </div>'
+            document.getElementById('mainView').innerHTML += '<div class="four columns"> <div style="margin: 0 auto; margin-top: 50%">'
+            document.getElementById('mainView').innerHTML += '<input class="u-full-width" v-model="thoughtId" disabled type="text" value="'+snapshot.key+'">'
+            document.getElementById('mainView').innerHTML += '<input class="u-full-width" v-model="thoughtTitle" type="text" value="'+snapshot.val().title+'" id="postTitle">'
+            document.getElementById('mainView').innerHTML += '<textarea class="u-full-width" type="text" value="'+snapshot.val().post+'" id="post">'
+            document.getElementById('mainView').innerHTML += '<input id="submitNewInfo" style="background-color: #FF1744; border-color: #FF1744;" v-on:click="submitNewInfo" class="button-primary u-full-width" type="submit" value="Update">'
+            document.getElementById('mainView').innerHTML += '</div></div>'
+            document.getElementById('mainView').innerHTML += '<div class="three columns"> <p></p> </div>'
+          })
         }
       })
     },
@@ -61,11 +78,34 @@ var rootView = new Vue({
       db.ref('thoughts/'+this.thoughtId).once('value', function(snapshot) {
         if (snapshot.hasChild('title')) {
           notie.alert(1, 'Success!' , 2)
+          document.title = "YourThoughts 💡" + ' - ' + snapshot.key;
           document.getElementById('mainView').innerHTML = '';
-          document.getElementById('mainView').innerHTML = '<input class="u-full-width" type="text" value="'+snapshot.key+'" id="postTitle">'
+          document.getElementById('mainView').innerHTML += '<div class="three columns"> <p></p> </div>'
+          document.getElementById('mainView').innerHTML += '<div class="four columns"> <div style="margin: 0 auto; margin-top: 50%">'
+          document.getElementById('mainView').innerHTML += '<input class="u-full-width" v-model="thoughtId" disabled type="text" value="'+snapshot.key+'">'
+          document.getElementById('mainView').innerHTML += '<input class="u-full-width" v-model="thoughtTitle" type="text" value="'+snapshot.val().title+'" id="postTitle">'
+          document.getElementById('mainView').innerHTML += '<textarea class="u-full-width" type="text" value="'+snapshot.val().post+'" id="post">'
+          document.getElementById('mainView').innerHTML += '<input id="submitNewInfo" style="background-color: #FF1744; border-color: #FF1744;" v-on:click="submitNewInfo" class="button-primary u-full-width" type="submit" value="Update">'
+          document.getElementById('mainView').innerHTML += '</div></div>'
+          document.getElementById('mainView').innerHTML += '<div class="three columns"> <p></p> </div>'
         } else {
           notie.alert(3, 'Sorry, that does not exist.' , 2)
         }
+      })
+    },
+
+    submitNewInfo: function () {
+      var postId = this.thoughtId
+      var newTitle = this.thoughtTitle
+      db.ref('thoughts/'+this.thoughtId).once('value', function(snapshot) {
+          db.ref('thoughts/'+postId).set({
+            title: newTitle,
+            titleCanBeChanged: true,
+            postCanBeChange: true,
+            post: '',
+            author: '',
+            comments: {}
+          });
       })
     },
 
